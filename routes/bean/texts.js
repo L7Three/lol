9d9f9bbd23cd0d9965b35bc4d56154e1853c416c
刘青山 <2239641728@qq.com>
2021-05-24 23:04:57 +0800

    https://github.com/L7Three/lol

---
+++ routes/bean/texts.js
@@ -0,0 +1,11 @@
+class Score{
+    constructor(title,name,content){
+        this.title = title;
+        this.name = name;
+        this.content = content;
+        
+        
+    }
+}
+
+module.exports = Score;
--- routes/xdmin.js
+++ routes/xdmin.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('xdmin');
+var mysql = require("mysql");
+
+const Score = require('./bean/texts');
+
+
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  port: "3306",
+  database: "user"
 });
+
+
+router.get('/add', function(req, res, next) {
+  res.render('insert');
+});
+
+// router.get('/', function(req, res, next) {
+//   res.render('xdmin');
+// });
+
+router.get('/', function (req, res) {
+  connection.query("select * from article ", function (err, results, fields) {
+    res.render('xdmin', {
+      data: results
+    });
+
+  })
+});
+
+router.post('/add', (req, res) => {
+
+  let score = new Score(req.body.title, req.body.name, req.body.content)
+  connection.query("insert into article(title,name,content) value(?,?,?)", [score.title, score.name, score.content], (err, result, fields) => {
+    res.redirect('/xdmin')
+  })
+
+})
 
 module.exports = router;
