49fc26cb0755805c956db9ce05023eadbd10a53f
刘青山 <2239641728@qq.com>
2021-05-27 18:55:32 +0800

    https://github.com/L7Three/lol

--- routes/LOL2.js
+++ routes/LOL2.js
@@ -1,9 +1,27 @@
+const { render } = require('ejs');
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+var User = require("./bean/user");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('LOL2');
+
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  port: "3306",
+  database: "user"
 });
+
+router.get('/', function (req, res) {
+  connection.query("select * from article ORDER BY id DESC", function (err, results, fields) {
+    res.render('LOL2', {
+      data: results
+    })
+  })
+});
+
+
+
 
 module.exports = router;
