c32d8f0e0b92a84726d932aba97f94211b84d84a
刘青山 <2239641728@qq.com>
2021-06-01 16:06:33 +0800

    https://github.com/L7Three/lol

--- routes/LOL2.js
+++ routes/LOL2.js
@@ -2,8 +2,7 @@ const { render } = require('ejs');
 var express = require('express');
 var router = express.Router();
 var mysql = require("mysql");
-
-
+var comment = require("./bean/comment");
 
 let connection = mysql.createConnection({
   host: "localhost",
@@ -22,15 +21,35 @@ router.get('/', function (req, res) {
   })
 });
 
-
 router.get('/details/:id', (req, res) => {
   let sqlStr = `SELECT * FROM article WHERE ID ="${req.params.id}" `
     connection.query(sqlStr, (err, result) => {
       if (err) throw err;
       console.log(result);
-      res.render("details", { data: result })
+      connection.query("select * from comment ORDER BY id DESC", function (err, results, fields) {
+        res.render('details', {
+          data: result,
+          datas: results
+        })
+      })
+      // res.render("details", { data: result, })
   })
 })
+
+// router.get('/', function (req, res) {
+//   connection.query("select * from comment ORDER BY id DESC", function (err, results, fields) {
+//     res.render('comment', {
+//       data: results
+//     })
+//   })
+// });
+
+router.post('/details/:id/add', (req, res) => {
+  let co = new comment(req.body.name, req.body.comment)
+  connection.query("insert into comment(name,comment) value(?,?)", [co.name, co.comment], (err, result, fields) => {
+    res.redirect('/details/:id')
+  })
+});
 
 
 module.exports = router;
---
+++ routes/bean/comment.js
@@ -0,0 +1,8 @@
+class comment{
+    constructor(name,comment){
+        this.name = name;
+        this.comment = comment;
+    }
+}
+
+module.exports = comment;
